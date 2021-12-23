c235fb5295f1b5b5ab42c7d4019f66fc56f6a4db
qinxin1015 <qingxinxiao15@foxmail.com>
2021-12-23 17:57:27 +0800

    modify seq2seq

--- Seq2Seq/seq2seq.py
+++ Seq2Seq/seq2seq.py
@@ -8,6 +8,7 @@ import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import from_numpy
 import nltk
 import warnings
 warnings.filterwarnings('ignore')
@@ -16,53 +17,73 @@ from get_data import *
 from train_fn import * 
 
 class PlainEncoder(nn.Module):
-    def __init__(self, vocab_size, hidden_size, dropout=0.2):
+    def __init__(self, vocab_size, embedding_size, hidden_size, dropout=0.2):
+        #以英文为例，vocab_size=5493, hidden_size=100, dropout=0.2
         super(PlainEncoder, self).__init__()
-        self.embed = nn.Embedding(vocab_size, hidden_size)
-        self.rnn = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.embed = nn.Embedding(vocab_size, embedding_size) #这里的hidden_size为embedding_dim：一个单词的维度 
+        self.rnn = nn.GRU(embedding_size, hidden_size, batch_first = True)      
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, lengths):
-        sorted_len, sorted_idx = lengths.sort(0, descending=True)
-        x_sorted = x[sorted_idx.long()]
-        embedded = self.dropout(self.embed(x_sorted))
+    def forward(self, x, lengths): 
+        '''需要把最后一个hidden state取出来，需要知道长度，因为句子长度不一样
+        x: english sequence 
+        lengths: batch里每个句子的长度
         
-        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, 
-                                                    sorted_len.long().cpu().data.numpy(), 
-                                                    batch_first=True)
-        packed_out, hid = self.rnn(packed_embedded)
-        out, _ = nn.utils.rnn.pad_packed_sequence(packed_out, 
-                                                batch_first=True)
-        _, original_idx = sorted_idx.sort(0, descending=False)
-        out = out[original_idx.long()].contiguous()
+        x: [batch_size, seq_len] => embed 
+        => embedded:[batch_size, seq_len, embedding_size] => pack_padded 
+        => packed_embedded:[batch_size, batch_size*seq_len, embedding_size] => rnn 
+        => packed_out:[batch_size, batch_size*seq_len, hidden_size]; => pad_packed
+           hid:[1, batch_size, hidden_size] 
+        => out: [batch_size, seq_len, hidden_size], hid[[-1]] [1, batch_size, hidden_size]
+        '''
+        sorted_lengths, sorted_idx = lengths.sort(0, descending=True) # 按照长度降序排序
+        x_sorted = x[sorted_idx.long()] # [batch_size, seq_len]
+        embedded = self.dropout(self.embed(x_sorted)) # [batch_size, seq_len, embedding_size]
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded,  # embedding后的句子
+                                                            sorted_lengths.long().cpu().data.numpy(),  # 句子长度
+                                                            batch_first=True)
+        # packed_embedded [batch_size*seq_len, embedding_size]
+        packed_out, hid = self.rnn(packed_embedded) # [batch_size*seq_len, embedding_size], [batch_size, embedding_size]
+        # pad_packed： padding操作
+        out, _ = nn.utils.rnn.pad_packed_sequence(packed_out, batch_first = True) # [seq_len, embedding_size]
+        _, original_idx = sorted_idx.sort(0, descending=False)  # 按照原始idx升序排列
+        out = out[original_idx.long()].contiguous() # torch.contiguous()方法首先拷贝了一份张量在内存中的地址，然后将地址按照形状改变后的张量的语义进行排列。
         hid = hid[:, original_idx.long()].contiguous()
         return out, hid[[-1]]
-
+    
 class PlainDecoder(nn.Module):
-    def __init__(self, vocab_size, hidden_size, dropout=0.2):
+    '''基于中文输入和英文encoder的最后一个隐藏层的输出，得到最终的翻译语句
+    y [batch_size, seq_len] => embed
+    => y_embed: [batch_size, seq_len, embed_size] => pack_padded
+    => packed_seq: [batch_size, batch_size*seq_len, embed_size] => rnn
+    => packed_out: [batch_size, batch_size*seq_len, hidden_size] => pad_packed
+       hidden: [1, batch_size, hidden_size]
+    => unpacked_out: [batch_size, seq_len, hidden_size] => log_softmax
+    => output: [batch_size, seq_len, cn_vocab_size]
+       hidden: [1, batch_size, hidden_size]
+    '''  
+    def __init__(self, vocab_size, embedding_size, hidden_size, dropout = 0.2):
         super(PlainDecoder, self).__init__()
-        self.embed = nn.Embedding(vocab_size, hidden_size)
-        self.rnn = nn.GRU(hidden_size, hidden_size, batch_first=True)
-        self.out = nn.Linear(hidden_size, vocab_size)
+        self.embed = nn.Embedding(vocab_size, embedding_size)
+        self.rnn = nn.GRU(embedding_size, hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, vocab_size)  
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, y, y_lengths, hid):
-        sorted_len, sorted_idx = y_lengths.sort(0, descending=True)
-        y_sorted = y[sorted_idx.long()]
-        hid = hid[:, sorted_idx.long()]
-
-        y_sorted = self.dropout(self.embed(y_sorted)) # batch_size, output_length, embed_size
-
-        packed_seq = nn.utils.rnn.pack_padded_sequence(y_sorted, sorted_len.long().cpu().data.numpy(), batch_first=True)
-        out, hid = self.rnn(packed_seq, hid)
-        unpacked, _ = nn.utils.rnn.pad_packed_sequence(out, batch_first=True)
+        sorted_lengths, sorted_idx = y_lengths.sort(0, descending=True)
+        y_sorted, hid = y[sorted_idx.long()], hid[:, sorted_idx.long()] 
+        y_embed = self.dropout(self.embed(y_sorted))  # [batch_size, seq_len, embed_size]
+        packed_seq = nn.utils.rnn.pack_padded_sequence(y_embed, 
+                                                       sorted_lengths.long().cpu().data.numpy(), 
+                                                       batch_first=True)
+        packed_out, hid = self.rnn(packed_seq, hid) #加上隐藏层
+        unpacked_out, _ = nn.utils.rnn.pad_packed_sequence(packed_out, batch_first=True)
         _, original_idx = sorted_idx.sort(0, descending=False)
-        output_seq = unpacked[original_idx.long()].contiguous()
+        output_seq = unpacked_out[original_idx.long()].contiguous()
         hid = hid[:, original_idx.long()].contiguous()
-
         output = F.log_softmax(self.out(output_seq), -1)
-        return output, hid
-    
+        return output, hid  
+
 class PlainSeq2Seq(nn.Module):
     def __init__(self, encoder, decoder):
         super(PlainSeq2Seq, self).__init__()
@@ -70,22 +91,23 @@ class PlainSeq2Seq(nn.Module):
         self.decoder = decoder
         
     def forward(self, x, x_lengths, y, y_lengths):
-        encoder_out, hid = self.encoder(x, x_lengths)
-        output, hid = self.decoder(y=y,
-                    y_lengths=y_lengths,
-                    hid=hid)
+        encoder_out, encoder_hid = self.encoder(x, x_lengths)
+        output, decoder_hid = self.decoder(y = y, y_lengths = y_lengths, 
+                                        hid = encoder_hid)
         return output, None
-    
-    def translate(self, x, x_lengths, y, max_length=100):
-        encoder_out, hid = self.encoder(x, x_lengths)
+
+    def translate(self, x, x_lengths, y, max_length = 10):
         preds = []
-        batch_size = x.shape[0]
         attns = []
+        batch_size = x.shape[0]
+        encoder_out, encoder_hid = self.encoder(x, x_lengths)
         for i in range(max_length):
-            output, hid = self.decoder(y=y,
-                    y_lengths=torch.ones(batch_size).long().to(y.device),
-                    hid=hid)
-            preds.append(output.max(2)[1].view(batch_size, 1))
+            output, decoder_hid = self.decoder(y = y,
+                                    y_lengths = torch.ones(batch_size).long().to(y.device),
+                                    hid = encoder_hid) 
+            #刚开始循环bos作为模型的首个输入单词，后续更新y，下个预测单词的输入是上个输出单词
+            y = output.max(2)[1].view(batch_size, 1)
+            preds.append(y)
         return torch.cat(preds, 1), None
 
 class LanguageModelCriterion(nn.Module):
@@ -98,9 +120,7 @@ class LanguageModelCriterion(nn.Module):
         mask = mask.contiguous().view(-1, 1)
         output = -input.gather(1, target) * mask
         output = torch.sum(output) / torch.sum(mask)
-
         return output
-
 
 
 def main():
@@ -121,7 +141,7 @@ def main():
     dev_en_seq, dev_cn_seq = encode(dev_en, dev_cn, en_dict, cn_dict)
 
     # 把全部句子分成batch
-    batch_size = 64
+    batch_size = 32
     train_data = gen_examples(train_en_seq, train_cn_seq, batch_size)
     dev_data = gen_examples(dev_en_seq, dev_cn_seq, batch_size)
 
@@ -129,29 +149,35 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     en_vocab_size = len(en_dict)
     cn_vocab_size = len(cn_dict)
-    embed_size = hidden_size = 100
+    embedding_size = 100
+    hidden_size = 64
+    num_epochs= 1
     dropout = 0.1
-    encoder = PlainEncoder(vocab_size=en_vocab_size, 
+    encoder = PlainEncoder(vocab_size = en_vocab_size, 
+                      embedding_size = embedding_size,
                       hidden_size=hidden_size,
-                    dropout=dropout)
+                      dropout=dropout)
+
     decoder = PlainDecoder(vocab_size=cn_vocab_size, 
-                      hidden_size=hidden_size,
-                          dropout=dropout)
+                        embedding_size = embedding_size,
+                        hidden_size=hidden_size,
+                        dropout=dropout)
+
     model = PlainSeq2Seq(encoder, decoder)
     model = model.to(device)
     loss_fn = LanguageModelCriterion().to(device)
     optimizer = torch.optim.Adam(model.parameters())
 
     random.shuffle(train_data)
-    train(model, train_data, 
-        num_epochs= 30, 
+    model = train(model, train_data, 
+        num_epochs= num_epochs, 
         device = device, 
         loss_fn = loss_fn,
         optimizer = optimizer,
         valid_data = dev_data)
 
-    for i in range(1000,1020):
-        translate_dev(i)
+    for i in range(100,120):
+        translate_dev(model, device, dev_en_seq, inv_en_dict, dev_cn_seq, inv_cn_dict,cn_dict,i)
         print()
 
 
--- Seq2Seq/train_fn.py
+++ Seq2Seq/train_fn.py
@@ -1,82 +1,84 @@
 
+import numpy as np
 import torch
+from torch import from_numpy
 
 def train(model, data, num_epochs, loss_fn, optimizer, valid_data, device):
     for epoch in range(num_epochs):
-        total_num_words = total_loss = 0.
         model.train()
-        for it, (mb_x, mb_x_lengths, mb_y, mb_y_lengths) in enumerate(data):
-            mb_x = torch.from_numpy(mb_x).long().to(device)
-            mb_x_lengths = torch.from_numpy(mb_x_lengths).long().to(device)
-            mb_input = torch.from_numpy(mb_y[:,:-1]).long().to(device)
-            mb_out = torch.from_numpy(mb_y[:, 1:]).long().to(device)
-            mb_y_lengths = torch.from_numpy(mb_y_lengths-1).long().to(device)
-            mb_y_lengths[mb_y_lengths <= 0] = 1
-
-            mb_pred, attn = model(mb_x, mb_x_lengths, mb_input, mb_y_lengths)
-
-            mb_out_mask = torch.arange(mb_y_lengths.max().item(), device=device)[None, :] < mb_y_lengths[:, None]
-            mb_out_mask = mb_out_mask.float()
-            loss = loss_fn(mb_pred, mb_out, mb_out_mask)
-
-            num_words = torch.sum(mb_y_lengths).item()
+        total_num_words = total_loss = 0.
+        for it, (en_input, en_len, de_input, de_len) in enumerate(data):#（英文batch，英文长度，中文batch，中文长度）
+            en_input,en_len = from_numpy(en_input).to(device).long(), from_numpy(en_len).to(device).long()
+            #前n-1个单词作为输入，后n-1个单词作为输出，因为输入的前一个单词要预测后一个单词
+            de_input, output = from_numpy(de_input[:, :-1]).to(device).long(),from_numpy(de_input[:, 1:]).to(device).long()
+            de_len = from_numpy(de_len-1).to(device).long() # 输入输出的长度都减1
+            de_len[de_len<=0] = 1
+            pred, attn = model(en_input, en_len, de_input, de_len) # 返回的是类PlainSeq2Seq里forward函数的两个返回值
+            out_mask = torch.arange(de_len.max().item(), device=device)[None, :] < de_len[:, None]
+            out_mask = out_mask.float()
+            # out_mask就是LanguageModelCriterion的传入参数mask。
+            loss = loss_fn(pred, output, out_mask)
+            num_words = torch.sum(de_len).item() #一个batch里多少个单词
             total_loss += loss.item() * num_words
+            #总损失，loss计算的是均值损失，每个单词都是都有损失，所以乘以单词数
             total_num_words += num_words
-
+            # 更新模型
             optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 5.)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 5.) #为了防止梯度过大，设置梯度的阈值
             optimizer.step()
+            
+            if it % 200 == 0:
+                print("Epoch: %d, iteration: %d, loss: %.4f"%(epoch, it, loss.item()))
 
-            if it % 100 == 0:
-                print("epoch", epoch, "iteration", it, "loss", loss.item())
-        print("epoch", epoch, "training loss", total_loss/total_num_words)
-        if epoch % 5 == 0:
-            print("evaluating on dev...")
-            evaluate(model, valid_data, loss_fn = loss_fn, device = device)
+        print("\nEpoch: %d, Training loss: %.4f \n"%(epoch, total_loss/total_num_words))
+        if epoch % 10 == 0:
+            evaluate(model, data = valid_data, loss_fn = loss_fn, device = device) # 评估模型
+
+    return model
 
 def evaluate(model, data, loss_fn, device):
     model.eval()
     total_num_words = total_loss = 0.
     with torch.no_grad():
-        for it, (mb_x, mb_x_lengths, mb_y, mb_y_lengths) in enumerate(data):
-            mb_x = torch.from_numpy(mb_x).long().to(device)
-            mb_x_lengths = torch.from_numpy(mb_x_lengths).long().to(device)
-            mb_input = torch.from_numpy(mb_y[:,:-1]).long().to(device)
-            mb_out = torch.from_numpy(mb_y[:, 1:]).long().to(device)
-            mb_y_lengths = torch.from_numpy(mb_y_lengths-1).long().to(device)
-            mb_y_lengths[mb_y_lengths <= 0] = 1
+        for it, (en_input, en_len, de_input, de_len) in enumerate(data):
+            en_input = from_numpy(en_input).long().to(device)
+            en_len = from_numpy(en_len).long().to(device)
+            trans_input = from_numpy(de_input[:,:-1]).long().to(device)
+            output = from_numpy(de_input[:, 1:]).long().to(device)
+            de_len = from_numpy(de_len-1).long().to(device)
+            de_len[de_len <= 0] = 1
 
-            mb_pred, attn = model(mb_x, mb_x_lengths, mb_input, mb_y_lengths)
+            pred, attn = model(en_input, en_len, trans_input, de_len)
 
-            mb_out_mask = torch.arange(mb_y_lengths.max().item(), device=device)[None, :] < mb_y_lengths[:, None]
-            mb_out_mask = mb_out_mask.float()
-            loss = loss_fn(mb_pred, mb_out, mb_out_mask)
+            out_mask = torch.arange(de_len.max().item(), device=device)[None, :] < de_len[:, None]
+            out_mask = out_mask.float()
+            loss = loss_fn(pred, output, out_mask)
 
-            num_words = torch.sum(mb_y_lengths).item()
+            num_words = torch.sum(de_len).item()
             total_loss += loss.item() * num_words
             total_num_words += num_words
 
-    print("evaluation loss", total_loss/total_num_words)
+    print("evaluation loss %.4f"% (total_loss/total_num_words))
 
-def translate_dev(i):
-    model.eval()
-    en_sent = " ".join([inv_en_dict[word] for word in dev_en[i]])
+def translate_dev(model, device, dev_en, inv_en_dict, dev_cn, inv_cn_dict,cn_dict,i):
+    en_sent = " ".join([inv_en_dict[w] for w in dev_en[i]])
     print(en_sent)
-    print(" ".join([inv_cn_dict[word] for word in dev_cn[i]]))
-
-    sent = nltk.word_tokenize(en_sent.lower())
+    cn_sent = " ".join([inv_cn_dict[w] for w in dev_cn[i]])
+    print("".join(cn_sent))
+    mb_x = torch.from_numpy(np.array(dev_en[i]).reshape(1, -1)).long().to(device)
+    #把句子升维，并转换成tensor
+    mb_x_len = torch.from_numpy(np.array([len(dev_en[i])])).long().to(device)
+    #取出句子长度，并转换成tensor
     bos = torch.Tensor([[cn_dict["BOS"]]]).long().to(device)
-    mb_x = torch.Tensor([[en_dict.get(w, 0) for w in sent]]).long().to(device)
-    mb_x_len = torch.Tensor([len(sent)]).long().to(device)
-    
-    translation, attention = model.translate(mb_x, mb_x_len, bos)
+    translation, attn = model.translate(mb_x, mb_x_len, bos)
+    #这里传入bos作为首个单词的输入
+    #translation=tensor([[ 8,  6, 11, 25, 22, 57, 10,  5,  6,  4]])
     translation = [inv_cn_dict[i] for i in translation.data.cpu().numpy().reshape(-1)]
-
     trans = []
     for word in translation:
-        if word != "EOS":
-            trans.append(word)
+        if word != "EOS": # 把数值变成单词形式
+            trans.append(word) #
         else:
             break
-    print(" ".join(translation))
+    print("".join(trans))   
